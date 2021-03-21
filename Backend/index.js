7db4ee13c8dba6011a78a3e4690917b0902af445
Sandeep <saisandeepburra@gmail.com>
2021-03-21 13:37:57 +0530

    Minor change

--- Backend/index.js
+++ Backend/index.js
@@ -15,9 +15,11 @@ app.use("/api/Student", studentRoutes);
 
 app.use(express.json({ extended: false }));
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("frontend/build"));
+	app.use(express.static("../Frontend/build"));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+		res.sendFile(
+			path.resolve(__dirname, "..", "Frontend", "build", "index.html")
+		);
 	});
 }
 
